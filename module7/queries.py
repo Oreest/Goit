c172025f18164330e0bb976020d37bcac4405401
Orest <lav.orest@gmail.com>
2024-04-22 18:13:20 +0300

    module 7 commit, update query

--- module7/queries.py
+++ module7/queries.py
@@ -4,7 +4,7 @@ from app.db import session
 from app.db_models import Teacher, Student, Subject, Grade, Group
 
 
-def select_1():
+def query_1():
     """
     Знайти 5 студентів із найбільшим середнім балом з усіх предметів.
     :return: list[dict]
@@ -14,10 +14,10 @@ def select_1():
     return result
 
 
-def select_2(discipline_id: int):
+def query_2(discipline_id: int):
     """
     Знайти студента із найвищим середнім балом з певного предмета.
-    :return:
+    :return: list[dict]
     """
     result = session.query(Subject.name, Student.fullname, func.round(func.avg(Grade.grade), 2).label('avg_grade')) \
         .select_from(Grade).join(Student).join(Subject).filter(Subject.id == discipline_id) \
@@ -25,10 +25,10 @@ def select_2(discipline_id: int):
     return result
 
 
-def select_3(discipline_id: int):
+def query_3(discipline_id: int):
     """
     Знайти середній бал у групах з певного предмета.
-    :return:
+    :return: list[dict]
     """
     result = session.query(Subject.name, Group.name, func.round(func.avg(Grade.grade), 2).label('avg_grade')) \
         .select_from(Grade).join(Student).join(Subject).join(Group) \
@@ -37,10 +37,10 @@ def select_3(discipline_id: int):
     return result
 
 
-def select_4(group_id: int):
+def query_4(group_id: int):
     """
     Знайти середній бал на потоці (по всій таблиці оцінок).
-    :return:
+    :return: list[dict]
     """
     result = session.query(Group.name, func.round(func.avg(Grade.grade), 2).label('avg_grade')) \
         .select_from(Grade).join(Student).join(Group).filter(Group.id == group_id) \
@@ -48,20 +48,20 @@ def select_4(group_id: int):
     return result
 
 
-def select_5(teacher_id: int):
+def query_5(teacher_id: int):
     """
     Знайти які курси читає певний викладач.
-    :return:
+    :return: list[dict]
     """
     result = session.query(Teacher.fullname, Subject.name) \
         .select_from(Teacher).join(Subject).filter(Teacher.id == teacher_id).all()
     return result
 
 
-def select_6(group_id: int):
+def query_6(group_id: int):
     """
     Знайти список студентів у певній групі.
-    :return:
+    :return: list[dict]
     """
     result = session.query(Student.fullname) \
         .select_from(Student).join(Group).filter(Group.id == group_id) \
@@ -69,10 +69,10 @@ def select_6(group_id: int):
     return result
 
 
-def select_7(group_id: int, discipline_id: int):
+def query_7(group_id: int, discipline_id: int):
     """
     Знайти оцінки студентів у окремій групі з певного предмета.
-    :return:
+    :return: list[dict]
     """
     result = session.query(Student.fullname, Subject.name, Grade.grade).select_from(Grade) \
         .join(Student).join(Subject).join(Group) \
@@ -81,10 +81,10 @@ def select_7(group_id: int, discipline_id: int):
     return result
 
 
-def select_8(teacher_id: int):
+def query_8(teacher_id: int):
     """
     Знайти середній бал, який ставить певний викладач зі своїх предметів.
-    :return:
+    :return: list[dict]
     """
     result = session.query(Teacher.fullname, Subject.name, func.round(func.avg(Grade.grade), 2).label('avg_grade')) \
         .select_from(Teacher).join(Subject).join(Grade) \
@@ -93,10 +93,10 @@ def select_8(teacher_id: int):
     return result
 
 
-def select_9(student_id: int):
+def query_9(student_id: int):
     """
     Знайти список курсів, які відвідує певний студент.
-    :return:
+    :return: list[dict]
     """
     result = session.query(Subject.name).select_from(Grade) \
         .join(Student).join(Subject) \
@@ -105,10 +105,10 @@ def select_9(student_id: int):
     return result
 
 
-def select_10(student_id: int, teacher_id: int):
+def query_10(student_id: int, teacher_id: int):
     """
     Список курсів, які певному студенту читає певний викладач.
-    :return:
+    :return: list[dict]
     """
     result = session.query(Subject.name, Student.fullname, Teacher.fullname).select_from(Grade) \
         .join(Student).join(Subject).join(Teacher) \
@@ -118,13 +118,13 @@ def select_10(student_id: int, teacher_id: int):
 
 
 if __name__ == '__main__':
-    print(f'1.Знайти 5 студентів із найбільшим середнім балом з усіх предметів:\n{select_1()}\n')
-    print(f'2.Знайти студента із найвищим середнім балом з певного предмета:\n{select_2(1)}\n')
-    print(f'3.Знайти середній бал у групах з певного предмета:\n{select_3(1)}\n')
-    print(f'4.Знайти середній бал на потоці (по всій таблиці оцінок):\n{select_4(1)}\n')
-    print(f'5.Знайти які курси читає певний виклада:\n{select_5(1)}\n')
-    print(f'6.Знайти список студентів у певній групі:\n{select_6(1)}\n')
-    print(f'7.Знайти оцінки студентів у окремій групі з певного предмета:\n{select_7(1, 1)}\n')
-    print(f'8.Знайти середній бал, який ставить певний викладач зі своїх предметів:\n{select_8(1)}\n')
-    print(f'9.Знайти список курсів, які відвідує певний студент:\n{select_9(2)}\n')
-    print(f'10.Список курсів, які певному студенту читає певний викладач:\n{select_10(1, 2)}')
+    print(f'1.Знайти 5 студентів із найбільшим середнім балом з усіх предметів:\n{query_1()}\n')
+    print(f'2.Знайти студента із найвищим середнім балом з певного предмета:\n{query_2(1)}\n')
+    print(f'3.Знайти середній бал у групах з певного предмета:\n{query_3(1)}\n')
+    print(f'4.Знайти середній бал на потоці (по всій таблиці оцінок):\n{query_4(1)}\n')
+    print(f'5.Знайти які курси читає певний виклада:\n{query_5(1)}\n')
+    print(f'6.Знайти список студентів у певній групі:\n{query_6(1)}\n')
+    print(f'7.Знайти оцінки студентів у окремій групі з певного предмета:\n{query_7(1, 1)}\n')
+    print(f'8.Знайти середній бал, який ставить певний викладач зі своїх предметів:\n{query_8(1)}\n')
+    print(f'9.Знайти список курсів, які відвідує певний студент:\n{query_9(2)}\n')
+    print(f'10.Список курсів, які певному студенту читає певний викладач:\n{query_10(1, 2)}')
