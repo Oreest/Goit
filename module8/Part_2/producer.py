25774c194161561496d915b5718addcf1e8d0ceb
Orest <lav.orest@gmail.com>
2024-04-28 02:19:23 +0300

    -

--- module8/Part_2/producer.py
+++ module8/Part_2/producer.py
@@ -14,7 +14,12 @@ def create_user(quantity):
 
 
 credentials = pika.PlainCredentials('guest', 'guest')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    host='localhost',
+    port=5672,
+    credentials=credentials
+    )
+)
 channel = connection.channel()
 
 channel.exchange_declare(exchange='hw8_mock', exchange_type='direct')
